beb3c9ea66ef9ed8437a989305eddc3678b77765
developerbola <developerbola08@gmail.com>
2025-04-29 15:12:26 +0500

    #29 Add hashnote release

--- app/hashnote/releases/page.tsx
+++ app/hashnote/releases/page.tsx
@@ -15,17 +15,32 @@ const Releases = () => {
                   notes: "First release for Hashnote.",
                   link: "https://github.com/developerbola/hashnote-electron",
                 },
-              ].map(({ date, version, notes, link }) => (
-                <div key={version} className="flex gap-10 items-start">
-                  <span className="w-1/5 text-sm text-muted-foreground">
-                    {date}
-                  </span>
-                  <a href={link} className="w-[70px] font-mono text-sm underline">
-                    {version}
-                  </a>
-                  <p className="text-sm">{notes}</p>
-                </div>
-              ))}
+                {
+                  date: "Apr 29, 2025",
+                  version: "v2.0.0",
+                  notes:
+                    "Made with Tauri v1 for reduce size. Removed Github activity & date. Fixed missing empty rows.",
+                  link: "https://github.com/developerbola/hashnote/releases/download/hashnote/Hashnote.dmg",
+                },
+              ]
+                .reverse()
+                .map(({ date, version, notes, link }) => (
+                  <div
+                    key={version}
+                    className="flex flex-col sm:flex-row gap-4 sm:gap-6 items-start p-3"
+                  >
+                    <span className="w-[120px] text-sm text-muted-foreground">
+                      {date}
+                    </span>
+                    <a
+                      href={link}
+                      className="w-[70px] font-mono text-sm text-[#4da8ff] underline"
+                    >
+                      {version}
+                    </a>
+                    <p className="text-sm flex-1">{notes}</p>
+                  </div>
+                ))}
             </div>
           </div>
         </div>

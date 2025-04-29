7f7f13b8ba5abc2d14183c9ece7020a7c90d7103
developerbola <developerbola08@gmail.com>
2025-04-29 14:55:20 +0500

    #25 Add stars button to player hero

--- app/player/page.tsx
+++ app/player/page.tsx
@@ -1,5 +1,6 @@
 import PlayerDownloadButton from "@/components/PlayerDownloadButton";
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "LooksMinimal | Player",
@@ -17,8 +18,9 @@ const Player = () => {
 export default Player;
 
 const PlayerHero = () => {
+  const stars = getStars();
   return (
-    <section className="w-full h-screen py-24 px-6 flex flex-col items-center text-center bg-black text-white">
+    <section className="w-full pt-14 px-6 flex flex-col items-center text-center bg-black text-white">
       <video
         src="/preview/player-preview.webm"
         autoPlay
@@ -27,22 +29,26 @@ const PlayerHero = () => {
         playsInline
         className="w-full max-w-[350px] min-h-[200px] h-auto block mb-9 rounded-xl"
       />
-
       <h1 className="text-4xl md:text-5xl font-bold mb-4">
         Spotify Mini Player
       </h1>
       <p className="text-base md:text-lg max-w-xl mb-8 text-gray-300">
         A minimal, always-on-top Spotify widget for your desktop. Lightweight,
-        distraction-free, and made for focused listening.
+        distraction-free, and made for vibe listening.
         <br />
         <span className="block pt-4">
-          <span className="text-yellow-400">!!!</span> Guide: to move app use
-          Command + click to app
+          To move app use Command + click to app{" "}
+          <Link href="https://github.com/developerbola/spotify-player">
+            <button className="inline-flex items-center gap-1 rounded-lg border border-gray-800 bg-slate-950 px-2 py-1 text-sm font-medium text-gray-300 transition hover:bg-slate-800">
+              ⭐ Give Star{" "}
+              <span style={{ fontFamily: "var(--font-jetbrains)" }}>
+                {stars}
+              </span>
+            </button>
+          </Link>
         </span>
       </p>
-
-      <PlayerDownloadButton />
-
+      <PlayerDownloadButton />{" "}
       <p className="text-sm text-gray-500 max-w-sm mt-6 leading-relaxed">
         ⚠️ When you click a button and it doesn't respond the first time — this
         isn’t a bug. The first click focuses the app window, and the second
@@ -51,3 +57,19 @@ const PlayerHero = () => {
     </section>
   );
 };
+
+async function getStars() {
+  const res = await fetch(
+    "https://api.github.com/repos/developerbola/spotify-player",
+    {
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+      },
+      next: { revalidate: 3600 }, // revalidate every hour
+    }
+  );
+
+  if (!res.ok) return 0;
+  const data = await res.json();
+  return data.stargazers_count;
+}
